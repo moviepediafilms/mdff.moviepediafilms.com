228aea895d728b5180876631223b9b696df0e051
Zeeshan Khan <zkhan1093@gmail.com>
2020-06-11 12:00:50 +0530

    removed static files, moved to nginx

--- moviepedia/urls.py
+++ moviepedia/urls.py
@@ -14,40 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic.base import RedirectView
+from django.urls import path, include
 
 urlpatterns = [
-    re_path(
-        r"^favicon\.ico$",
-        RedirectView.as_view(url="/static/favicon.ico", permanent=True),
-    ),
-    re_path(
-        r"^icon\.png$", RedirectView.as_view(url="/static/icon.png", permanent=True)
-    ),
-    re_path(
-        r"^tile\.png$", RedirectView.as_view(url="/static/tile.png", permanent=True)
-    ),
-    re_path(
-        r"^tile-wide\.png$",
-        RedirectView.as_view(url="/static/tile-wide.png", permanent=True),
-    ),
-    re_path(
-        r"^robots\.txt$",
-        RedirectView.as_view(url="/static/robots.txt", permanent=True),
-    ),
-    re_path(
-        r"^humans\.txt$",
-        RedirectView.as_view(url="/static/humans.txt", permanent=True),
-    ),
-    re_path(
-        r"^site\.webmanifest$",
-        RedirectView.as_view(url="/static/site.webmanifest", permanent=True),
-    ),
-    re_path(
-        r"^browserconfig\.xml$",
-        RedirectView.as_view(url="/static/browserconfig.xml", permanent=True),
-    ),
     path("admin/", admin.site.urls),
     path("", include("dff2020.urls")),
 ]
