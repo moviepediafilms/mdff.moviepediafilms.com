e6ca3101d94083a781029d78eff59c8eeeed3be7
Zeeshan Khan <zkhan1093@gmail.com>
2020-06-28 21:11:53 +0530

    adding unit test

--- dff2020/tests.py
+++ dff2020/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.contrib import auth
 
-# Create your tests here.
+
+app_name = "dff2020"
+
+
+class LogoutTest(TestCase):
+    def setUp(self):
+        self.client.login(username="fred", password="secret")
+
+    def test_logout(self):
+        res = self.client.get(reverse(f"{app_name}:logout"))
+        self.assertRedirects(res, reverse("dff2020:login"))
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated)
+
+
+class LoginTest(TestCase):
+    def test_get(self):
+        res = self.client.get(reverse(f"{app_name}:login"))
+        self.assertTemplateUsed(res, template_name="dff2020/login.html")
+        print(dir(res.content))
+
+    def test_post(self):
+        self.client.post(reverse(f"{app_name}:login"))
